901e09eb9b9a5088218ae937113449f5a34847c8
Nirmalya <nirmalyaganguly1999@gmail.com>
2025-08-19 00:28:20 +0530

    added mui customizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,46 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
-import ProductDetailPage from "./pages/ProductDetail";
-import RootLayout from "./pages/RootLayout";
-import Register from "./pages/Register";
-import { ToastContainer } from "react-toastify";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import { useEffect } from "react";
-import { useAppDispatch } from "./store/hooks";
-import { getProfileThunk } from "./store/thunk/auth";
-import { getCartByIdThunk, getCurrentUserCartThunk } from "./store/thunk/cart";
-import { CARTID_KEY } from "./constants/cart.constants";
-import Checkout from "./pages/Checkout";
-import CheckAuth from "./components/guards/CheckAuth";
-import { authActions } from "./store/slice/authSlice";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductDetailPage from './pages/ProductDetail';
+import RootLayout from './pages/RootLayout';
+import Register from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import { useEffect } from 'react';
+import { useAppDispatch } from './store/hooks';
+import { getProfileThunk } from './store/thunk/auth';
+import { getCartByIdThunk, getCurrentUserCartThunk } from './store/thunk/cart';
+import { CARTID_KEY } from './constants/cart.constants';
+import Checkout from './pages/Checkout';
+import CheckAuth from './components/guards/CheckAuth';
+import { authActions } from './store/slice/authSlice';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme/theme';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "products",
-        children: [{ path: ":slug", element: <ProductDetailPage /> }],
+        path: 'products',
+        children: [{ path: ':slug', element: <ProductDetailPage /> }],
       },
       {
-        path: "register",
+        path: 'register',
         element: <Register />,
       },
       {
-        path: "login",
+        path: 'login',
         element: <Login />,
       },
       {
-        path: "cart",
+        path: 'cart',
         element: <Cart />,
       },
       {
-        path: "checkout",
+        path: 'checkout',
         element: (
           <CheckAuth>
             <Checkout />,
@@ -54,15 +56,12 @@ const App = () => {
 
   useEffect(() => {
     (async () => {
-      const accessToken = localStorage.getItem("accessToken");
+      const accessToken = localStorage.getItem('accessToken');
       const anonymousCartId = localStorage.getItem(CARTID_KEY);
       try {
         // If accessToken is present, then make API calls to get logged in users profile and cart
         if (accessToken) {
-          await Promise.all([
-            dispatch(getProfileThunk()).unwrap(),
-            dispatch(getCurrentUserCartThunk()).unwrap(),
-          ]);
+          await Promise.all([dispatch(getProfileThunk()).unwrap(), dispatch(getCurrentUserCartThunk()).unwrap()]);
           localStorage.removeItem(CARTID_KEY);
           return;
         } else {
@@ -80,10 +79,10 @@ const App = () => {
   }, []);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <ToastContainer />
       <RouterProvider router={router} />;
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  spacing: 10,
+  palette: {
+    primary: {
+      main: '#0D0D0D',
+      contrastText: '#ffffff',
+    },
+  },
+});
+
+export default theme;
