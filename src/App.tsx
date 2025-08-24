78613ca0b6b90b63c6fbec68208e129a1d96ae71
Nirmalya <nirmalyaganguly1999@gmail.com>
2025-08-24 16:26:49 +0530

    Make profile and get current cart api calls sequential

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
-import ProductDetailPage from "./pages/ProductDetail";
-import RootLayout from "./pages/RootLayout";
-import Register from "./pages/Register";
-import { ToastContainer } from "react-toastify";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import { useEffect } from "react";
-import { useAppDispatch } from "./store/hooks";
-import { getProfileThunk } from "./store/thunk/auth";
-import { getCartByIdThunk, getCurrentUserCartThunk } from "./store/thunk/cart";
-import { CARTID_KEY } from "./constants/cart.constants";
-import Checkout from "./pages/Checkout";
-import PublicRoute from "./components/guards/PublicRoute";
-import ProtectedRoute from "./components/guards/ProtectedRoute";
-import { authActions } from "./store/slice/authSlice";
-import { ThemeProvider } from "@emotion/react";
-import theme from "./theme/theme";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductDetailPage from './pages/ProductDetail';
+import RootLayout from './pages/RootLayout';
+import Register from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import { useEffect } from 'react';
+import { useAppDispatch } from './store/hooks';
+import { getProfileThunk } from './store/thunk/auth';
+import { getCartByIdThunk, getCurrentUserCartThunk } from './store/thunk/cart';
+import { CARTID_KEY } from './constants/cart.constants';
+import Checkout from './pages/Checkout';
+import PublicRoute from './components/guards/PublicRoute';
+import ProtectedRoute from './components/guards/ProtectedRoute';
+import { authActions } from './store/slice/authSlice';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme/theme';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "products",
-        children: [{ path: ":slug", element: <ProductDetailPage /> }],
+        path: 'products',
+        children: [{ path: ':slug', element: <ProductDetailPage /> }],
       },
       {
-        path: "register",
+        path: 'register',
         element: <Register />,
       },
       {
-        path: "login",
+        path: 'login',
         element: (
           <PublicRoute>
             <Login />
@@ -41,11 +41,11 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "cart",
+        path: 'cart',
         element: <Cart />,
       },
       {
-        path: "checkout",
+        path: 'checkout',
         element: (
           <ProtectedRoute>
             <Checkout />,
@@ -61,16 +61,18 @@ const App = () => {
 
   useEffect(() => {
     (async () => {
-      const accessToken = localStorage.getItem("accessToken");
+      const accessToken = localStorage.getItem('accessToken');
       const anonymousCartId = localStorage.getItem(CARTID_KEY);
       try {
         // If accessToken is present, then make API calls to get logged in users profile and cart
         if (accessToken) {
-          await Promise.all([
-            dispatch(getProfileThunk()).unwrap(),
-            dispatch(getCurrentUserCartThunk()).unwrap(),
-          ]);
-          localStorage.removeItem(CARTID_KEY);
+          // To do: Handle Scenario when both requests return 401 unauthorized and hence token is refreshed twice
+          // await Promise.all([
+          //   dispatch(getProfileThunk()).unwrap(),
+          //   dispatch(getCurrentUserCartThunk()).unwrap(),
+          // ]);
+          await dispatch(getProfileThunk()).unwrap();
+          await dispatch(getCurrentUserCartThunk()).unwrap(), localStorage.removeItem(CARTID_KEY);
           return;
         } else {
           // Reset loading flag to false if user is not fetched in get profile call
